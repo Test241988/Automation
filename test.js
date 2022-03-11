686aa7334227e03221ea386ccb86137e8e04db60
Ankur Chadha <ankurchadha01@gmail.com>
2022-03-11 14:01:42 -0800

    test

---
+++ test.js
@@ -0,0 +1,6 @@
+import errorHandling from "./features/support/error_handler.js";
+import controller from "./features/support/controller.js";
+
+fixture("fixture")
+test
+("test", controller.capture)
