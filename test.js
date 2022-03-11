e9f437f9323b7f34764bca253e1cfd8985671b15
Ankur Chadha <ankurchadha01@gmail.com>
2022-03-11 14:33:53 -0800

    updated workflow file

--- test.js
+++
@@ -1,6 +0,0 @@
-import errorHandling from "./features/support/error_handler.js";
-import controller from "./features/support/controller.js";
-
-fixture("fixture")
-test
-("test", controller.capture)
